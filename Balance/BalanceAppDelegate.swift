6a7c6efda7fbe29a9bf0c2572d1b58eb09a59ac5
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-02-03 08:28:16 -0800

    Initial Project Setup (#1)

---
+++ Balance/BalanceAppDelegate.swift
@@ -0,0 +1,39 @@
+//
+// This source file is part of the CS342 2023 Balance Team Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import BalanceMockDataStorageProvider
+import BalanceSchedule
+import CardinalKit
+import FHIR
+import HealthKit
+import HealthKitDataSource
+import HealthKitToFHIRAdapter
+import Questionnaires
+import Scheduler
+import SwiftUI
+
+
+class BalanceAppDelegate: CardinalKitAppDelegate {
+    override var configuration: Configuration {
+        Configuration(standard: FHIR()) {
+            if HKHealthStore.isHealthDataAvailable() {
+                HealthKit {
+                    CollectSample(
+                        HKQuantityType(.stepCount),
+                        deliverySetting: .anchorQuery(.afterAuthorizationAndApplicationWillLaunch)
+                    )
+                } adapter: {
+                    HealthKitToFHIRAdapter()
+                }
+            }
+            QuestionnaireDataSource()
+            MockDataStorageProvider()
+            BalanceScheduler()
+        }
+    }
+}
