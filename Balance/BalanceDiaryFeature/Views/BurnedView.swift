c2cd66795929eb98a24543245a0384b87a099404
lowbaj21 <lowbaj21@stanford.edu>
2023-02-27 01:53:55 -0800

    added note burning animation

---
+++ Balance/BalanceDiaryFeature/Views/BurnedView.swift
@@ -0,0 +1,67 @@
+//
+//  BurnedView.swift
+//  Balance
+//
+//  Created by Alexis Lowber on 2/26/23.
+//
+//code based on: https://betterprogramming.pub/how-to-create-amazing-animations-in-swiftui-3c2970ee6e0
+
+import SwiftUI
+import UIKit
+
+/*var fire_animation_images: [UIImage] = load("landmarkData.json")
+*/
+
+struct BurnedView: View {
+    @State var frameImage: String = ""
+    //@Environment(\.dismiss) var dismiss
+    @Binding var burningNote: Bool
+    //@Binding var burnComplete: Bool
+    @Binding var text: String
+    
+    var body: some View {
+        VStack(alignment: .center){
+            Image(frameImage)
+                .resizable()
+                .frame(width: 400, height: 875, alignment: .center)
+                //.scaledToFill()
+                .aspectRatio(contentMode: .fill)
+                .onAppear(perform: updateFrame)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+    
+    func updateFrame() {
+        var frameIndex = 1
+        let numFrames = 29
+        let speed = 0.08
+        
+        _ = Timer.scheduledTimer(withTimeInterval: speed, repeats: true, block: { (Timer) in
+            frameImage = "burningPaper\(frameIndex)"
+            frameIndex += 1
+            if (frameIndex > numFrames) {
+                Timer.invalidate()
+                burningNote.toggle()
+                text = "This is some editable text..."
+            }
+        });
+    }
+}
+
+struct BurnedView_Previews: PreviewProvider {
+    static var previews: some View {
+        BurnedView(burningNote: .constant(true), text: .constant("Hi"))
+    }
+}
+
+/*func animatedImages(for name: String) -> [UIImage] {
+    
+    var i = 0
+    var images = [UIImage]()
+    
+    while let image = UIImage(named: "\(name)/\(i)") {
+        images.append(image)
+        i += 1
+    }
+    return images
+*/
--- Balance/BalanceDiaryFeature/Views/DiaryNoteEntryView.swift
+++ Balance/BalanceDiaryFeature/Views/DiaryNoteEntryView.swift
@@ -14,51 +14,61 @@ struct DiaryNoteEntryView: View {
     @State private var title = ""
     @State private var text: String = "This is some editable text..."
     @State private var savedNotes: [Note] = []
+    @State private var burningNote = false
+    @State private var burnComplete = false
+    //@EnvironmentObject private var burningNote: Bool
     
     var body: some View {
-        VStack {
-            TextField("Note Title", text: $title)
-                .padding()
-            
-            TextEditor(text: $text)
-                .foregroundColor(self.text == "This is some editable text..." ? .gray : .primary)
-//TextEditor doesn't allow placeholder text so found this patch online
-                .onTapGesture {
-                    if self.text == "This is some editable text..." {
-                        self.text = ""
+        ZStack {
+            VStack {
+                TextField("Note Title", text: $title)
+                    .padding()
+                
+                TextEditor(text: $text)
+                    .foregroundColor(self.text == "This is some editable text..." ? .gray : .primary)
+                //TextEditor doesn't allow placeholder text so found this patch online
+                    .onTapGesture {
+                        if (self.text == "This is some editable text...") {
+                            self.text = ""
+                        }
+                        burnComplete.toggle()
+                    }
+                    .padding()
+                
+                HStack(spacing: 100) {
+                    Button("Save") {
+                        let note = Note(
+                            id: UUID().uuidString,
+                            title: title,
+                            text: text,
+                            date: Date()
+                        )
+                        vm.notes.append(note)
+                        title = ""
+                        text = ""
+                        
+                        Task {
+                            await vm.saveToStorage()
+                        }
+                    }.buttonStyle(.borderedProminent)
+                    
+                    Button("Burn") {
+                        burningNote.toggle()
                     }
                 }
+                .frame(maxWidth: .infinity, alignment: .center)
                 .padding()
-            
-            HStack(spacing: 100) {
-                Button("Save") {
-                    let note = Note(
-                        id: UUID().uuidString,
-                        title: title,
-                        text: text,
-                        date: Date()
-                    )
-                    vm.notes.append(note)
-                    title = ""
-                    text = ""
-
-                    Task {
-                        await vm.saveToStorage()
+                // found online how to get notes saved right below
+                List(savedNotes, id: \.title) { note in
+                    VStack(alignment: .leading) {
+                        Text(note.title)
+                            .font(.headline)
+                        Text(note.text)
                     }
-                }.buttonStyle(.borderedProminent)
-
-                Button("Burn") {
                 }
             }
-            .frame(maxWidth: .infinity, alignment: .center)
-            .padding()
- // found online how to get notes saved right below
-            List(savedNotes, id: \.title) { note in
-                VStack(alignment: .leading) {
-                    Text(note.title)
-                        .font(.headline)
-                    Text(note.text)
-                }
+            if burningNote {
+                BurnedView(burningNote: $burningNote, text: $text)
             }
         }
     }
