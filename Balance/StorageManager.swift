bd621dd4277c5a0d5becdc2496047cda829d5ead
lowbaj21 <lowbaj21@stanford.edu>
2023-03-05 19:01:12 -0800

    making storage manager file

---
+++ Balance/StorageManager.swift
@@ -0,0 +1,18 @@
+//
+//  StorageManager.swift
+//  Balance
+//
+//  Created by Alexis Lowber on 3/5/23.
+//
+
+//import Foundation
+import FirebaseCore
+//import FirebaseStorage
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    func uploadUserActivity() {
+        
+    }
+}
