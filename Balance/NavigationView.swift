5124bfc5b8630325c0ec9961b18640e58b654fda
Adam Zhao <adamzhao@stanford.edu>
2023-03-07 18:26:27 -0800

    Fixed remaining errors due to duplicates within the pbxproj file

--- Balance/NavigationView.swift
+++ Balance/NavigationView.swift
@@ -12,8 +12,8 @@ struct NavView: View {
     var image: String
     var text: String
     
-    let fcolor = Color(UIColor(red: 0.25, green: 0.38, blue: 0.50, alpha: 1.00))
-    let bcolor = Color(UIColor(red: 0.30, green: 0.79, blue: 0.94, alpha: 0.05))
+    let fcolor = Color(red: 0.25, green: 0.38, blue: 0.50, opacity: 1.00)
+    let bcolor = Color(red: 0.30, green: 0.79, blue: 0.94, opacity: 0.05)
     
     var body: some View {
         HStack {
