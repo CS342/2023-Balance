d149575553ff6641581924410c4cb447ecfc17f7
Daniel Guo <dguo8412@gmail.com>
2023-03-13 00:53:40 -0700

    Finalized Meditation Feature Graphics

--- Balance/Meditation/MeditationSpotifyView.swift
+++ Balance/Meditation/MeditationSpotifyView.swift
@@ -11,33 +11,46 @@ import UIKit
 
 struct MeditationSpotifyView: View {
     @Environment(\.openURL) var openURL
-    let spotifyURLArray = [
-        "https://open.spotify.com/track/7doeCRERLqKcAzEufAi44e?si=6fa0c7e046b4461e",
-        "https://open.spotify.com/track/2JHBuWOMSGHOWCoLrXAriB?si=5f6d601ac0664d93",
-        "https://open.spotify.com/track/46biZBPkwqTzB70Pabb9ks?si=f315155074864e1d",
-        "https://open.spotify.com/track/3wdROXnP25gPoEKLyubpqc?si=e1400690922a48e4",
-        "https://open.spotify.com/track/5aP4bQuGcZIWzSoGGQeN0S?si=e7730ac057c34bfa",
-        "https://open.spotify.com/track/1buvmZ3wdzJ2I7mqd1ifNs?si=da278ed097b64953",
-        "https://open.spotify.com/track/3fd1dXUQ92rRnxKWyqgwcx?si=44e40d0ffd384cd9"
+    
+    //FOLLOWED A SPOTIFY YOUTUBE TUTORIAL
+    struct Song: Identifiable {
+        var id: String { title }
+        let title: String
+        let artist: String
+        let coverString: String
+        let spotifyURL: String
+    }
+    
+    let songs = [
+        Song(title: "Emotion Management 1", artist: "To calm you down", coverString:"meditationThumbnail 2", spotifyURL: "https://open.spotify.com/track/2JHBuWOMSGHOWCoLrXAriB?si=5f6d601ac0664d93"),
+        Song(title: "Emotion Management 2", artist: "To calm you down", coverString:"meditationThumbnail 3", spotifyURL: "https://open.spotify.com/track/46biZBPkwqTzB70Pabb9ks?si=f315155074864e1d"),
+        Song(title: "Emotion Managemen 3", artist: "To calm you down", coverString:"meditationThumbnail 1", spotifyURL: "https://open.spotify.com/track/3wdROXnP25gPoEKLyubpqc?si=e1400690922a48e4"),
+        Song(title: "Emotion Management 4", artist: "To calm you down", coverString:"meditationThumbnail 2", spotifyURL:  "https://open.spotify.com/track/1buvmZ3wdzJ2I7mqd1ifNs?si=da278ed097b64953"),
+        Song(title: "Emotion Managemen 5t", artist: "To calm you down", coverString:"meditationThumbnail 3", spotifyURL: "https://open.spotify.com/track/5aP4bQuGcZIWzSoGGQeN0S?si=e7730ac057c34bfa"),
+        Song(title: "Emotion Management 6", artist: "To calm you down", coverString:"meditationThumbnail 1", spotifyURL: "https://open.spotify.com/track/3fd1dXUQ92rRnxKWyqgwcx?si=44e40d0ffd384cd9"),
+        Song(title: "Emotion Management 7", artist: "To calm you down", coverString:"meditationThumbnail 2", spotifyURL:  "https://open.spotify.com/track/1buvmZ3wdzJ2I7mqd1ifNs?si=da278ed097b64953"),
+        Song(title: "Emotion Management 8", artist: "To calm you down", coverString:"meditationThumbnail 1", spotifyURL: "https://open.spotify.com/track/7doeCRERLqKcAzEufAi44e?si=6fa0c7e046b4461e")
     ]
     
     var body: some View {
         ScrollView(.vertical) {
             VStack {
-                ForEach(spotifyURLArray, id: \.self) { spotifyID in
-                    if let url = URL(string: spotifyID) {
+                ForEach(songs) { song in
+                    if let url = URL(string: song.spotifyURL) {
                         Button(action: {
                             UIApplication.shared.open(url)
                         }) {
                             HStack {
-                                Image("BalanceLogo")
+                                Image(song.coverString)
                                     .resizable()
+                                    .frame(width: 130, height: 130, alignment: .leading)
                                     .aspectRatio(contentMode: .fill)
-                                    .frame(width: 150, height: 20)
-                                    .accessibilityLabel(Text("Balance Logo"))
-                                Text("Open in Spotify").font(.custom("Nunito-Bold", size: 25))
+                                VStack(alignment: .leading) {
+                                    Text(song.title).font(.custom("Nunito-Bold", size: 15))
+                                    Text(song.artist).font(.custom("Nunito-Bold", size: 10))
+                                }
                             }
-                            .frame(width: 360, height: 200)
+                            .frame(width: 360, height: 120, alignment: .leading)
                             .foregroundColor(Color(#colorLiteral(red: 0.25, green: 0.38, blue: 0.50, alpha: 1.00)))
                             .background(Color(#colorLiteral(red: 0.30, green: 0.79, blue: 0.94, alpha: 0.05)))
                             .cornerRadius(40)
--- Balance/Meditation/SleepView.swift
+++ Balance/Meditation/SleepView.swift
@@ -11,33 +11,45 @@ import SwiftUI
 struct SleepView: View {
     @Environment(\.openURL) var openURL
     
-    let sleepURLArray = [
-        "https://open.spotify.com/track/5NWOEqOSCqHebuRMjYEn1i?si=bc627872f8d14624",
-        "https://open.spotify.com/track/475L4j7qFaZE0jlcvrsScN?si=04f4eb003ce94c66",
-        "https://open.spotify.com/track/0N5NNIaNs3YVhiyt22ZYhG?si=5ecc0e2fc507461f",
-        "https://open.spotify.com/track/0CMYUXTTTmI6Lwc0opH2XG?si=3399143e6a154fb9",
-        "https://open.spotify.com/track/5F820xSuKiMLpGOV04Xs3c?si=8601ea70e87744d8",
-        "https://open.spotify.com/track/6MePLoqcI97WjWyx5Sq2JV?si=6cee5d6e859542ad",
-        "https://open.spotify.com/track/1SucXU3xTOCyBAAqE22E8B?si=3147950d3de04a52"
+    //FOLLOWED A SPOTIFY YOUTUBE TUTORIAL
+    struct Song: Identifiable {
+        var id: String { title }
+        let title: String
+        let artist: String
+        let coverString: String
+        let spotifyURL: String
+    }
+    
+    let songs = [
+        Song(title: "Sleep 1", artist: "To help you sleep", coverString:"meditationThumbnail 1", spotifyURL: "https://open.spotify.com/track/2sr8v2aNZ7lZ8tV3eAR5qt?si=bbbfa9d35a104ec7"),
+        Song(title: "Sleep 2", artist: "To help you sleep", coverString:"meditationThumbnail 2", spotifyURL: "https://open.spotify.com/track/475L4j7qFaZE0jlcvrsScN?si=04f4eb003ce94c66"),
+        Song(title: "Sleep 3", artist: "To help you sleep", coverString:"meditationThumbnail 3", spotifyURL: "https://open.spotify.com/track/0N5NNIaNs3YVhiyt22ZYhG?si=5ecc0e2fc507461f"),
+        Song(title: "Sleep 4", artist: "To help you sleep", coverString:"meditationThumbnail 1", spotifyURL: "https://open.spotify.com/track/0CMYUXTTTmI6Lwc0opH2XG?si=3399143e6a154fb9"),
+        Song(title: "Sleep 5", artist: "To help you sleep", coverString:"meditationThumbnail 2", spotifyURL:  "https://open.spotify.com/track/5F820xSuKiMLpGOV04Xs3c?si=8601ea70e87744d8"),
+        Song(title: "Sleep 6", artist: "To help you sleep", coverString:"meditationThumbnail 3", spotifyURL: "https://open.spotify.com/track/6MePLoqcI97WjWyx5Sq2JV?si=6cee5d6e859542ad"),
+        Song(title: "Sleep 7", artist: "To help you sleep", coverString:"meditationThumbnail 1", spotifyURL: "https://open.spotify.com/track/1SucXU3xTOCyBAAqE22E8B?si=3147950d3de04a52"),
+        Song(title: "Sleep 8", artist: "To help you sleep", coverString:"meditationThumbnail 2", spotifyURL:  "https://open.spotify.com/track/5NWOEqOSCqHebuRMjYEn1i?si=bc627872f8d14624")
     ]
     
     var body: some View {
         ScrollView(.vertical) {
             VStack {
-                ForEach(sleepURLArray, id: \.self) { spotifyID in
-                    if let url = URL(string: spotifyID) {
+                ForEach(songs) { song in
+                    if let url = URL(string: song.spotifyURL) {
                         Button(action: {
                             UIApplication.shared.open(url)
-                        }) {
+                        }){
                             HStack {
-                                Image("BalanceLogo")
+                                Image(song.coverString)
                                     .resizable()
+                                    .frame(width: 130, height: 130, alignment: .leading)
                                     .aspectRatio(contentMode: .fill)
-                                    .frame(width: 150, height: 20)
-                                    .accessibilityLabel(Text("Balance Logo"))
-                                Text("Open in Spotify").font(.custom("Nunito-Bold", size: 25))
+                                VStack(alignment: .leading) {
+                                    Text(song.title).font(.custom("Nunito-Bold", size: 15))
+                                    Text(song.artist).font(.custom("Nunito-Bold", size: 10))
+                                }
                             }
-                            .frame(width: 360, height: 200)
+                            .frame(width: 360, height: 120, alignment: .leading)
                             .foregroundColor(Color(#colorLiteral(red: 0.25, green: 0.38, blue: 0.50, alpha: 1.00)))
                             .background(Color(#colorLiteral(red: 0.30, green: 0.79, blue: 0.94, alpha: 0.05)))
                             .cornerRadius(40)
