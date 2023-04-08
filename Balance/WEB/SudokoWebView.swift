143e6e0b6fa8cdc1598891c5b5441f826d50f2e7
gperissetcelteeka <gonzalo.perisset@celteeka.com>
2023-04-08 11:01:10 -0300

    [BAL-53]-[GP]-Added new feature (sudoko game) on distraction view.

---
+++ Balance/WEB/SudokoWebView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentWebView.swift
+//  Balance
+//
+//  Created by Gonzalo Perisset on 06/04/2023.
+//
+
+import SwiftUI
+
+struct SudokuWebView: View {
+    @StateObject var webViewStore = WebViewStore()
+    
+    var body: some View {
+        HeaderMenu(title: "Sudoku")
+        WebView(webView: webViewStore.webView)
+            .edgesIgnoringSafeArea(.bottom)
+            .onAppear {
+                guard let sudokuLink = URL(string: "https://sudoku.com/") else {
+                    return
+                }
+                self.webViewStore.webView.load(URLRequest(url: sudokuLink))
+            }
+    }
+    
+    func goBack() {
+        webViewStore.webView.goBack()
+    }
+    
+    func goForward() {
+        webViewStore.webView.goForward()
+    }
+}
+struct ContentWebView_Previews: PreviewProvider {
+    static var previews: some View {
+        SudokuWebView()
+    }
+}
