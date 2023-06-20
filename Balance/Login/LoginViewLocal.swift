2c8d0c3a2a867e3d28fabf8baaba87336e7f9563
gperissetcelteeka <129070838+gperissetcelteeka@users.noreply.github.com>
2023-06-20 16:01:55 -0300

    BAL-XX-GP-Swiftlint (#128)

--- Balance/Login/LoginViewLocal.swift
+++ Balance/Login/LoginViewLocal.swift
@@ -12,6 +12,7 @@ import FirebaseAccount
 import Onboarding
 import SwiftUI
 
+// swiftlint:disable multiline_arguments
 struct LoginViewLocal: View {
     @EnvironmentObject private var authModel: AuthViewModel
     @EnvironmentObject var account: Account
