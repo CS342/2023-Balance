8a1205a153f6d545c8f4f8f02c2cb7f8e9114f23
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-09-28 10:29:18 -0700

    Update

--- BalanceWatchApp/ContentView.swift
+++ BalanceWatchApp/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
+                .accessibilityHidden(true)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
