5f8024fb0dc5b105c5be796b4f7b322a47f529f3
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-09-27 18:57:30 -0700

    Add watchOS App

---
+++ BalanceWatchApp/BalanceWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceWatchAppApp.swift
+//  BalanceWatchApp Watch App
+//
+//  Created by Paul Shmiedmayer on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct BalanceWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
