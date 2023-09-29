c263d3ec2fe4279be9c60f25caf8923475ef5959
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-09-28 17:53:50 -0700

    Add watchOS App (#179)

---
+++ BalanceWatchApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BalanceWatchApp Watch App
+//
+//  Created by Paul Shmiedmayer on 9/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .accessibilityHidden(true)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
